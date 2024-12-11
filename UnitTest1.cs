dcefb2c86022eb6b50b354fe2b0e1ed50a268777
QWQ916 <Shirok2005@icloud.com>
2024-12-11 23:07:47 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,26 @@
+using Moq;
+
+namespace TestProject1
+{
+    public class BankAccountTest
+    {
+        private BankAccount _account;
+        private Mock<BankAccount> _mockBankAccount;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockBankAccount = new Mock<BankAccount>();
+            _account = new BankAccount(1, 0);
+        }
+        [Test]
+        public void Deposit_Success()
+        {
+            _mockBankAccount.Setup(p => p.getbalance()).Returns(1000.42);
+
+            var dep = _account.getbalance();
+
+            Assert.That(dep, Is.EqualTo(1000.42));
+        }
+    }
+}
